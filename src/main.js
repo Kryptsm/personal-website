cd72754c2c6b1cbe8b16523d4587edea6fa73768
Kryptsm <kryptsmfed@gmail.com>
2023-08-04 00:41:34 -0500

    feat: Implement router and flower game

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import Personal from "./components/personal/Personal.vue";
+import FlowerGame from "./components/flower-game/FlowerGame.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount("#app");
+const routes = [
+	{
+		path: "/",
+		name: "personal",
+		component: Personal,
+	},
+	{
+		path: "/flower-game",
+		name: "flower-game",
+		component: FlowerGame,
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+const app = createApp(App);
+app.use(router);
+
+app.mount("#app");
