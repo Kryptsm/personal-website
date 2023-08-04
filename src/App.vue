1373089840d19b2d5dcdabbf3419b11315ab1fd8
Kryptsm <kryptsmfed@gmail.com>
2023-08-03 21:50:40 -0500

    feat: Initial implementation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import PersonalBanner from "./components/PersonalBanner.vue";
+import ProfileActions from "./components/ProfileActions.vue";
+import Notification from "./components/Notification.vue";
+
+export default {
+	components: {
+		Header,
+		PersonalBanner,
+		ProfileActions,
+		Notification,
+	},
+	data() {
+		return {};
+	},
+};
+</script>
+
+<template>
+	<Header />
+	<PersonalBanner />
+	<ProfileActions />
+</template>
+
+<style scoped></style>
