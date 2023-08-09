e7355a93028c5fdd1f04d6327e84d38a26942936
Kryptsm <kryptsmfed@gmail.com>
2023-08-08 22:11:26 -0500

    feat: Adding gradient to maze bubble

--- src/components/maze-solver/components/Tile.vue
+++ src/components/maze-solver/components/Tile.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useGetRandomInt } from "../../../functions/math";
 import { ref } from "vue";
-const props = defineProps(["status", "x", "y", "hoverStatus"]);
+const props = defineProps(["status", "x", "y", "hoverStatus", "trackerNum"]);
 const emits = defineEmits(["select-choice"]);
 </script>
 
@@ -22,7 +22,15 @@ const emits = defineEmits(["select-choice"]);
 	<div class="tile start" v-if="status == 2"></div>
 	<div class="tile end" v-if="status == 3"></div>
 	<div class="tile path" v-if="status == 4"></div>
-	<div class="tile bubble" v-if="status == 5"></div>
+	<div
+		class="tile bubble"
+		:style="{
+			backgroundColor: `rgb(${130 - trackerNum * 1.55}, ${
+				200 - trackerNum * 2.2
+			}, ${230 - trackerNum * 2})`,
+		}"
+		v-if="status == 5"
+	></div>
 </template>
 
 <style scoped lang="scss">
@@ -48,10 +56,6 @@ const emits = defineEmits(["select-choice"]);
 
 .path {
 	background-color: yellowgreen;
-}
-
-.bubble {
-	background-color: lightblue;
 }
 
 .space,
