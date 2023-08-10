3ac14375faf00b4a822b841bea005cb1bb7dcbc4
Kryptsm <kryptsmfed@gmail.com>
2023-08-10 14:08:19 -0500

    fix: Slight margin fix

--- src/components/personal/components/PersonalBanner.vue
+++ src/components/personal/components/PersonalBanner.vue
@@ -53,7 +53,7 @@ export default {
 				<div
 					class="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1"
 				>
-					<div class="mt-6 min-w-0 flex-1 sm:hidden md:block name-and-work">
+					<div class="mt-10 min-w-0 flex-1 sm:hidden md:block name-and-work">
 						<h1 class="truncate text-2xl font-bold text-gray-900">
 							{{ profile.name }}
 						</h1>
