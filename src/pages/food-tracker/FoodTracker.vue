65282880bc19045bcfa62a3c735e5fc6be81f898
Kryptsm <kryptsmfed@gmail.com>
2023-08-16 15:44:55 -0500

    feat: Adding in previous weeks defaulted to 3

--- src/pages/food-tracker/FoodTracker.vue
+++ src/pages/food-tracker/FoodTracker.vue
@@ -9,12 +9,14 @@ import { Pie } from "vue-chartjs";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const showWeeks = ref(3);
 const daysOfTheWeek = ref([
 	{
 		name: "Sunday",
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -22,6 +24,7 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -29,6 +32,7 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -36,6 +40,7 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -43,6 +48,7 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -50,6 +56,7 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 	{
@@ -57,20 +64,22 @@ const daysOfTheWeek = ref([
 		date: undefined,
 		isToday: false,
 		isFuture: false,
+		currentWeek: true,
 		meals: [],
 	},
 ]);
+const previousWeeks = ref([]);
 const graphColors = ref([
 	"#52D726",
-	"#FFEC00",
 	"#FF7300",
-	"#FF0000",
 	"#007ED6",
 	"#7CDDDD",
+	"#FFEC00",
 	"#963868",
 	"#727394",
 	"#853277",
 	"#A37730",
+	"#FF0000",
 ]);
 const allMeals = ref([]);
 const allRestaurants = ref([]);
@@ -82,23 +91,79 @@ const pieOptions = ref({
 });
 
 onMounted(() => {
-	// var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-	// let localDate = new Date(Date.now() - tzoffset);
-	// console.log("Date: ", localDate);
-	// var localISODate = localDate.toISOString().split("T")[0];
+	for (let x = 0; x < showWeeks.value - 1; x++) {
+		previousWeeks.value = [
+			...previousWeeks.value,
+			[
+				{
+					name: "Sunday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Monday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Tuesday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Wednesday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Thursday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Friday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+				{
+					name: "Saturday",
+					date: undefined,
+					isToday: false,
+					isFuture: false,
+					currentWeek: false,
+					meals: [],
+				},
+			],
+		];
+	}
 
-	// console.log("ISO Date: ", localISODate);
+	console.log("Previous Weeks: ", previousWeeks.value);
 
-	let date = new Date(); // Or the date you'd like converted.
+	let date = new Date();
 	let isoDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
 		.toISOString()
 		.split("T")[0];
 	let currentDay = date.getDay();
-	console.log("Date: ", date);
-	console.log("ISO Date: ", isoDate);
-	console.log("Day: ", currentDay);
 
-	daysOfTheWeek.value[currentDay].isToday = true;
+	daysOfTheWeek.value.filter((e) => e.currentWeek)[currentDay].isToday = true;
 
 	daysOfTheWeek.value.forEach((e, index) => {
 		if (index > currentDay) {
@@ -127,6 +192,23 @@ onMounted(() => {
 		}
 	});
 
+	previousWeeks.value.forEach((week, weekIndex) => {
+		week.forEach((day, dayIndex) => {
+			let distanceFromCurrentDay =
+				7 - dayIndex + 7 * (showWeeks.value - 2 - weekIndex) + currentDay;
+
+			let newDate = new Date(date);
+			newDate.setDate(newDate.getDate() - distanceFromCurrentDay);
+			let isoNewDate = new Date(
+				newDate.getTime() - newDate.getTimezoneOffset() * 60000
+			)
+				.toISOString()
+				.split("T")[0];
+
+			day.date = isoNewDate;
+		});
+	});
+
 	func.listRestaurants().then((result) => {
 		allRestaurants.value = result;
 		getPieData(totalRestaurants(result));
@@ -139,9 +221,11 @@ onMounted(() => {
 });
 
 function addNewMeals(meals) {
-	//console.log(meals);
 	meals.forEach((meal) => {
 		let day = daysOfTheWeek.value.find((day) => day.date == meal.date);
+		previousWeeks.value.forEach((week) => {
+			if (!day) day = week.find((day) => day.date == meal.date);
+		});
 		if (day) day.meals = [...day.meals, meal];
 	});
 }
@@ -165,7 +249,32 @@ function totalMeals(items) {
 			}
 		}
 	});
-	console.log("Meal Counts: ", itemCounts);
+	return itemCounts;
+}
+
+function totalSides(items) {
+	let itemCounts = [];
+	items.forEach((item) => {
+		if (item.side) {
+			let existingTracker = itemCounts.find(
+				(count) => count.name === item.side
+			);
+
+			if (!existingTracker) {
+				itemCounts.push({ name: item.side, count: 1 });
+			} else if (existingTracker) {
+				existingTracker.count++;
+			}
+		} else {
+			let existingTracker = itemCounts.find((count) => count.name === "None");
+
+			if (!existingTracker) {
+				itemCounts.push({ name: "None", count: 1 });
+			} else if (existingTracker) {
+				existingTracker.count++;
+			}
+		}
+	});
 	return itemCounts;
 }
 
@@ -174,7 +283,6 @@ function totalRestaurants(items) {
 	items.forEach((item) => {
 		itemCounts.push({ name: item.name, count: item.Meals.items.length });
 	});
-	console.log("Restaurant Counts: ", itemCounts);
 	return itemCounts;
 }
 
@@ -188,120 +296,163 @@ function getPieData(itemCounts) {
 		];
 		newData.datasets[0].data = [...newData.datasets[0].data, item.count];
 	});
-
-	console.log(newData);
 	return newData;
 }
 </script>
 
 <template>
-	<div class="week-calendar">
-		<div class="day-card" v-for="day in daysOfTheWeek">
-			<div class="name" :class="{ today: day.isToday }">
-				<div class="dayName">{{ day.name }}</div>
-			</div>
-			<div class="content">
-				<div class="dayNum">{{ getDayNum(day.date) }}</div>
-				<div v-if="!day.isFuture">
-					<div class="meal" v-for="meal in day.meals">
-						<span v-if="!simplifiedNames">{{ meal.name }}</span>
-						<span v-if="simplifiedNames">{{ meal.coreName }}</span>
+	<div class="food-tracker">
+		<div class="week-calendar">
+			<div class="day-card" v-for="(day, index) in daysOfTheWeek">
+				<div class="name">
+					<div class="dayName">{{ day.name }}</div>
+				</div>
+				<div class="previousWeeks" v-if="showWeeks > 1">
+					<div class="week" v-for="prevWeek in previousWeeks">
+						<div class="content">
+							<div class="dayNum">{{ getDayNum(prevWeek[index].date) }}</div>
+							<div>
+								<div class="meal" v-for="meal in prevWeek[index].meals">
+									<span v-if="!simplifiedNames">{{ meal.name }}</span>
+									<span v-if="simplifiedNames">{{ meal.coreName }}</span>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div v-if="!day.isToday && day.isFuture" class="future">
-					<img :src="FutureSpongebob" />
+				<div class="content" :class="{ today: day.isToday }">
+					<div class="dayNum">{{ getDayNum(day.date) }}</div>
+					<div v-if="!day.isFuture">
+						<div class="meal" v-for="meal in day.meals">
+							<span v-if="!simplifiedNames">{{ meal.name }}</span>
+							<span v-if="simplifiedNames">{{ meal.coreName }}</span>
+						</div>
+					</div>
+					<div v-if="!day.isToday && day.isFuture" class="future">
+						<img :src="FutureSpongebob" />
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<h6 class="graph-heading">Totals:</h6>
-	<div class="graph">
-		<div class="pie-container">
-			<p>Restaurants Visited</p>
-			<Pie
-				:data="getPieData(totalRestaurants(allRestaurants))"
-				:options="pieOptions"
-			/>
-		</div>
-		<div class="pie-container">
-			<p>Meals Eaten</p>
-			<Pie :data="getPieData(totalMeals(allMeals))" :options="pieOptions" />
+		<h6 class="graph-heading">Statistics:</h6>
+		<div class="graph">
+			<div class="pie-container">
+				<p>Restaurants Visited</p>
+				<Pie
+					:data="getPieData(totalRestaurants(allRestaurants))"
+					:options="pieOptions"
+				/>
+			</div>
+			<div class="pie-container">
+				<p>Meals Eaten</p>
+				<Pie :data="getPieData(totalMeals(allMeals))" :options="pieOptions" />
+			</div>
+			<div class="pie-container">
+				<p>Sides Eaten</p>
+				<Pie :data="getPieData(totalSides(allMeals))" :options="pieOptions" />
+			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped lang="scss">
-.week-calendar {
-	display: flex;
-	justify-content: space-around;
-	width: 95%;
-	margin: 15px auto;
+.food-tracker {
+	margin: 0 0 35px 0;
+	.week-calendar {
+		display: flex;
+		justify-content: space-around;
+		width: 95%;
+		margin: 15px auto;
 
-	.day-card {
-		border: 1px solid black;
-		width: 100%;
-		text-align: center;
+		.day-card {
+			border: 1px solid black;
+			width: 100%;
+			text-align: center;
 
-		.today {
-			background-color: #bee3f1;
-		}
-
-		.name {
-			padding: 12px 0;
-			border-radius: 0;
-		}
-
-		.content {
-			border-top: 1px solid black;
-			border-radius: 0;
-			position: relative;
-			padding: 25px 10px 10px 10px;
-			height: 140px;
-
-			.dayNum {
-				position: absolute;
-				top: 0;
-				left: 5px;
+			.today {
+				background-color: #bee3f1 !important;
 			}
 
-			@media only screen and (max-width: 900px) {
-				.future {
-					display: none;
+			.name {
+				padding: 12px 0;
+				border-radius: 0;
+				background-color: rgb(238, 104, 104);
+				font-weight: 700;
+			}
+
+			.content {
+				border-top: 1px solid black;
+				border-radius: 0;
+				position: relative;
+				padding: 25px 10px 10px 10px;
+				height: 140px;
+				background-color: rgb(250, 250, 250);
+
+				.dayNum {
+					position: absolute;
+					top: 0;
+					left: 5px;
+				}
+
+				@media only screen and (max-width: 900px) {
+					.future {
+						display: none;
+					}
+				}
+
+				.meal + .meal {
+					margin-top: 10px;
 				}
 			}
 		}
+
+		:first-child {
+			border-top-left-radius: 6px;
+			border-bottom-left-radius: 6px;
+
+			.name {
+				border-top-left-radius: 6px;
+			}
+
+			:last-child {
+				border-bottom-left-radius: 6px;
+			}
+		}
+
+		:not(:first-child) {
+			border-left: none;
+		}
+
+		:last-child {
+			border-top-right-radius: 6px;
+			border-bottom-right-radius: 6px;
+
+			.name {
+				border-top-right-radius: 6px;
+			}
+
+			:last-child {
+				border-bottom-right-radius: 6px;
+			}
+		}
 	}
 
-	:first-child {
-		border-top-left-radius: 6px;
-		border-bottom-left-radius: 6px;
+	.graph-heading {
+		width: 95%;
+		margin: auto;
 	}
 
-	:not(:first-child) {
-		border-left: none;
-	}
+	.graph {
+		display: flex;
+		justify-content: space-around;
+		width: 100%;
 
-	:last-child {
-		border-top-right-radius: 6px;
-		border-bottom-right-radius: 6px;
-	}
-}
+		.pie-container {
+			width: 40%;
 
-.graph-heading {
-	width: 95%;
-	margin: auto;
-}
-
-.graph {
-	display: flex;
-	justify-content: space-around;
-	width: 100%;
-
-	.pie-container {
-		width: 40%;
-
-		p {
-			text-align: center;
+			p {
+				text-align: center;
+			}
 		}
 	}
 }
