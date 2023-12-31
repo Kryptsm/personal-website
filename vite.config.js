4d64cd2cd1510afd409357c09c9ad1aa1ea4c5e4
Kryptsm <kryptsmfed@gmail.com>
2023-12-30 22:06:28 -0600

    fix: More attempts

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
-const http = require("http");
-const fs = require("fs");
-const httpPort = 80;
-
-http
-	.createServer((req, res) => {
-		fs.readFile("index.html", "utf-8", (err, content) => {
-			if (err) {
-				console.log('We cannot open "index.html" file.');
-			}
-
-			res.writeHead(200, {
-				"Content-Type": "text/html; charset=utf-8",
-			});
-
-			res.end(content);
-		});
-	})
-	.listen(httpPort, () => {
-		console.log("Server listening on: http://localhost:%s", httpPort);
-	});
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/",
